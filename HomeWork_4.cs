be12b357946045a7959fd9c3735abc43c48ed1a7
kriuchkovaleks <103981431+kriuchkovaleks@users.noreply.github.com>
2022-04-28 11:43:56 +0400

    Add files via upload

---
+++ HomeWork_4.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Input random number");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1; 
+
+Console.WriteLine("Even numbers from 1 to number are: ");
+
+while (i < number)
+{
+    if ((i % 2) == 0)
+    {
+        
+        Console.WriteLine(i);
+
+    }
+
+    i = i + 1;
+
+}
