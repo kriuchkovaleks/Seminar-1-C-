729049806eb66da145e05c5857c96c7c6ce70ea0
kriuchkovaleks <103981431+kriuchkovaleks@users.noreply.github.com>
2022-04-28 11:42:48 +0400

    Add files via upload

---
+++ HomeWork_3.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Input random number");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number % 2) == 0)
+{
+
+Console.WriteLine("Even number");
+
+}
+
+else 
+{
+    Console.WriteLine("Not Even number");
+}
+
+
